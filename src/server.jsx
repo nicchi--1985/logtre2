3cd493e021d541781b818971c65f1086be1b880f
nicchi <nicchi.achan@gmail.com>
2016-04-29 23:06:39 +0900

    init logtre app

---
+++ src/server.jsx
@@ -0,0 +1,11 @@
+import express from 'express'
+const app = express()
+
+app.get('/', function(req, res) {
+  console.log('got request')
+  res.send(`<html><body>Hello world</body></html>`);
+});
+
+app.listen(3000, function() {
+  console.log('app listening on port 3000 ...')
+});
---
+++ webpack.config.babel.js
@@ -0,0 +1,50 @@
+import 'babel-polyfill';
+import path from 'path';
+import webpack from 'webpack';
+
+const DEBUG = !process.argv.includes('--release');
+const VERBOSE = process.argv.includes('--verbose');
+
+export default {
+  cache: DEBUG,
+  debug: DEBUG,
+
+  stats: {
+    colors: true,
+    reasons: DEBUG,
+    hash: VERBOSE,
+    version: VERBOSE,
+    timings: true,
+    chunks: VERBOSE,
+    chunkModules: VERBOSE,
+    cached: VERBOSE,
+    cachedAssets: VERBOSE,
+  },
+  target: "node",
+  entry: path.join(__dirname, "/src/server.jsx"),
+  output: {
+    path: path.join(__dirname, "/public"),
+    filename: "server.js"
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx'],
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        include: [path.resolve(__dirname, 'src')],
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.json$/,
+        loader: 'json-loader'
+      }
+    ],
+  },
+  node: {
+    fs: "empty",
+    net: "empty"
+  }
+};
